daf4daa6682b502c3098508390f6d7a5bed4d441
Marcelo Finamor Vieira <marcelofinamorvieira@gmail.com>
2023-07-25 11:21:49 +0200

    Fix

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -10,7 +10,7 @@ const AboutPage = () => {
         pageName="About Page"
         description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In varius eros eget sapien consectetur ultrices. Ut quis dapibus libero."
       />
-      <Team />
+      {/* <Team /> */}
     </>
   );
 };
