7d82be4aada4045057380319245e2c8ba8b0a0f8
Silvano Stralla <silvano.stralla@sistrall.it>
2023-07-28 18:24:36 +0200

    Upgrade Next version and change Image import

--- components/Blog/SingleBlog.tsx
+++ components/Blog/SingleBlog.tsx
@@ -1,6 +1,6 @@
 import { Blog } from '@/types/blog';
 import Link from 'next/link';
-import { Image } from 'react-datocms/image';
+import { Image as DatoImage } from "react-datocms";
 
 const SingleBlog = ({ blog }) => {
   const { title, seoTags, description, author, tags, publishDate, slug } = blog;
@@ -15,7 +15,7 @@ const SingleBlog = ({ blog }) => {
           <span className="absolute top-6 right-6 z-20 inline-flex items-center justify-center rounded-full bg-primary py-2 px-4 text-sm font-semibold capitalize text-white">
             {tags[0].tag}
           </span>
-          <Image data={seoTags.image.responsiveImage} />
+          <DatoImage data={seoTags.image.responsiveImage} />
         </Link>
         <div className="p-6 sm:p-8 md:py-8 md:px-6 lg:p-8 xl:py-8 xl:px-5 2xl:p-8">
           <h3>
