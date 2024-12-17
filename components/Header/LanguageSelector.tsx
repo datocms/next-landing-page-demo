7f086ca2ee982af23998b0639d1da6c73647294f
Roger Tuan <r.tuan@datocms.com>
2024-12-17 12:20:21 -0800

    Language selector width

--- components/Header/LanguageSelector.tsx
+++ components/Header/LanguageSelector.tsx
@@ -11,7 +11,7 @@ type Props = {
   languages: SiteLocale[];
 };
 
-const localeToLanguageName = (locale: SiteLocale): string => {
+export const localeToLanguageName = (locale: SiteLocale): string => {
   const normalizedLocale = locale.replaceAll('_', '-');
   return (
     new Intl.DisplayNames([normalizedLocale], { type: 'language' }).of(
@@ -40,18 +40,18 @@ const LanguageSelector = ({ globalPageProps, languages }: Props) => {
             setIsOpen(false);
           }, 100)
         }
-        className="ml-4 inline-flex w-28 items-center overflow-hidden rounded-md bg-white transition duration-100 hover:bg-gray-200 active:scale-95 active:bg-gray-300"
+        className="ml-4 w-40 inline-flex items-center overflow-hidden rounded-md bg-white transition duration-100 hover:bg-gray-200 active:scale-95 active:bg-gray-300 text-center"
       >
         <button
           type="button"
-          className="inline-flex cursor-pointer items-center justify-center rounded-lg px-4 py-2 text-sm font-medium text-gray-800"
+          className="inline-flex cursor-pointer items-center justify-center rounded-lg px-4 py-2 text-sm font-medium text-gray-800 w-full"
         >
-          🌐 {localeToLanguageName(currentLocale)}
+          {localeToLanguageName(currentLocale)}
         </button>
       </div>
 
       <div
-        className={`absolute end-0 z-10 ml-4 mt-1 w-28 rounded-md border border-gray-100 bg-white shadow-lg${
+        className={`absolute w-40 end-0 z-10 ml-4 mt-1 rounded-md border border-gray-100 bg-white shadow-lg${
           isOpen ? '' : ' hidden'
         }`}
         role="menu"
@@ -64,7 +64,7 @@ const LanguageSelector = ({ globalPageProps, languages }: Props) => {
             >
               <Link
                 href={buildUrl({ params: { locale } }, pathnameWithoutPrefix)}
-                className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white"
+                className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white w-full text-center"
                 role="menuitem"
               >
                 <div className="inline-flex">
